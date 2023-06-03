3435b747017f0ef580503ace5d7e186db61bdecf
albina97ka <albina97ka@gmail.com>
2023-06-03 09:29:26 +0300

    init commit

--- src/Veterinarian.java
+++
@@ -1,8 +0,0 @@
-public class Veterinarian{
-    public void treatAnimal(Animal animal) {
-        animal.makeNoise();
-        animal.eat();
-        animal.sleep();
-        System.out.println();
-    }
-}
