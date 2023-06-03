37d2c0316d67f74e1a08ca405acaaa07334940a3
albina97ka <albina97ka@gmail.com>
2023-06-03 09:39:03 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,57 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("корм", "дом", "корги");
-        Cat cat = new Cat("корм", "квартира", 9);
-        Horse horse = new Horse("корм", "стойло", "коричневый");
+        File inputFile = new File("input.txt");
+        File outputFile = new File("output.txt");
 
-        Veterinarian vet = new Veterinarian();
-        vet.treatAnimal(dog);
-        vet.treatAnimal(cat);
-        vet.treatAnimal(horse);
+        try {
+            Scanner scanner = new Scanner(inputFile);
+            PrintWriter writer = new PrintWriter(outputFile);
+
+            boolean inMultilineComment = false; // флаг для отслеживания многострочных комментариев
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                // проверяем, не начинается ли строка с многострочного комментария
+                if (line.trim().startsWith("/*")) {
+                    inMultilineComment = true;
+                }
+
+                if (!inMultilineComment) {
+                    // удаляем однострочные комментарии
+                    line = line.replaceAll("//.*", "");
+                    writer.println(line);
+                } else {
+                    // проверяем, заканчивается ли строка многострочным комментарием
+                    if (line.trim().endsWith("*/")) {
+                        inMultilineComment = false;
+                    }
+                }
+
+                // удаляем любые оставшиеся многострочные комментарии
+                if (inMultilineComment) {
+                    line = "";
+                } else {
+                    // удаляем многострочные комментарии внутри строки
+                    Pattern pattern = Pattern.compile("/\\*.*?\\*/", Pattern.DOTALL);
+                    Matcher matcher = pattern.matcher(line);
+                    line = matcher.replaceAll("");
+                }
+
+                writer.println(line);
+            }
+
+            scanner.close();
+            writer.close();
+
+            System.out.println("Комментарии успешно удалены и код записан в файл output.txt");
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении/записи файлов: " + e.getMessage());
+        }
     }
 }
